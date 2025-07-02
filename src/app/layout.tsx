cf415d531874c27bae93da1b1e8088aaa6f163a2
土井薫平 <kumpei@KmpMacBook-Pro.local>
2025-07-02 16:31:55 +0900

    add analytic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,14 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header"; // 作成したHeaderをインポート
 import Footer from '@/components/Footer'; // 作成したFooterをインポート
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
 // サイトのメタ情報を設定
 export const metadata: Metadata = {
   title: "Kumpei.com - くんぺい公式サイト",
-  description: "Next.jsで構築された、くんぺいの公式サイトです。",
+  description: "くんぺいの公式サイトです。",
 };
 
 export default function RootLayout({
@@ -23,6 +24,7 @@ export default function RootLayout({
         <Header /> {/* ここにHeaderを配置 */}
         <main>{children}</main> {/* childrenをmainタグで囲むとより良い */}
         <Footer />
+        {process.env.NODE_ENV === 'production' && <Analytics/>}
       </body>
     </html>
   );
